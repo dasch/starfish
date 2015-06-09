b2e64bdfc88834ce2b6d9ca494cc5b057d951c07
Daniel Schierbeck <dasch@zendesk.com>
2015-06-09 12:42:20 +0200

    Add an example of creating a channel

--- spec/requests/setup_spec.rb
+++ spec/requests/setup_spec.rb
@@ -18,6 +18,21 @@ describe "Project Setup" do
     expect(last_request.url).to eq "http://example.org/projects/skynet/production"
   end
 
+  example "adding a channel" do
+    create_project name: "Skynet", repo: "dasch/dummy"
+    create_pipeline project: "skynet", pipeline_name: "Production", pipeline_branch: "master"
+
+    create_channel(
+      project: "skynet",
+      pipeline: "production",
+      channel_name: "Staging",
+      channel_auto_release: "1"
+    )
+
+    get "/projects/skynet/production/channels/staging/releases"
+    expect(last_response.status).to eq 200
+  end
+
   def create_project(**params)
     post "/setup", params
     follow_redirect!
@@ -25,6 +40,11 @@ describe "Project Setup" do
 
   def create_pipeline(project:, **params)
     post "/projects/#{project}/pipelines", params
+    follow_redirect!
+  end
+
+  def create_channel(project:, pipeline:, **params)
+    post "/projects/#{project}/#{pipeline}/channels", params
     follow_redirect!
   end
 
