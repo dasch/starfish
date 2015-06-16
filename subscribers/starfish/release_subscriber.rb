18a79f53414ae8b0eb7a2827471ffa1249b6d2e1
Daniel Schierbeck <dasch@zendesk.com>
2015-06-16 12:22:29 +0200

    Refactor ReleaseSubscriber

--- subscribers/starfish/release_subscriber.rb
+++ subscribers/starfish/release_subscriber.rb
@@ -11,88 +11,41 @@ module Starfish
     end
 
     def build_automatically_released(timestamp, data)
-      data = data[:release]
-      project = @repo.find_project(data[:project_id])
-      pipeline = project.find_pipeline(data[:pipeline_id])
-      channel = pipeline.find_channel(data[:channel_id])
-
-      build = pipeline.find_build_by_number(data[:build_number])
-      config = channel.find_config(version: data[:config_version])
-
-      author = User.new(
-        name: data[:author][:name],
-        username: data[:author][:username],
-        avatar_url: data[:author][:avatar_url],
-      )
-
-      release = channel.add_release(
-        id: data[:id],
-        build: build,
-        config: config,
-        author: author,
-        event: AutomaticReleaseEvent.new(build: build)
-      )
+      build = find_build(data[:release])
+      event = AutomaticReleaseEvent.new(build: build)
+      release = add_release(data[:release], event: event)
 
       $logger.info "Automatically added release #{release}"
     end
 
     def build_released(timestamp, data)
-      data = data[:release]
-      project = @repo.find_project(data[:project_id])
-      pipeline = project.find_pipeline(data[:pipeline_id])
-      channel = pipeline.find_channel(data[:channel_id])
-
-      build = pipeline.find_build_by_number(data[:build_number])
-      config = channel.find_config(version: data[:config_version])
-
-      author = User.new(
-        name: data[:author][:name],
-        username: data[:author][:username],
-        avatar_url: data[:author][:avatar_url],
-      )
-
-      release = channel.add_release(
-        id: data[:id],
-        build: build,
-        config: config,
-        author: author,
-        event: ManualReleaseEvent.new(build: build)
-      )
+      build = find_build(data[:release])
+      event = ManualReleaseEvent.new(build: build)
+      release = add_release(data[:release], event: event)
 
       $logger.info "Added release #{release}"
     end
 
     def rollback_released(timestamp, data)
-      target_release_id = data[:target_release_id]
-      data = data[:release]
-      project = @repo.find_project(data[:project_id])
-      pipeline = project.find_pipeline(data[:pipeline_id])
-      channel = pipeline.find_channel(data[:channel_id])
+      target_release = find_release(data[:release], data[:target_release_id])
+      event = RollbackEvent.new(target_release: target_release)
 
-      target_release = channel.find_release(target_release_id)
-      build = pipeline.find_build_by_number(data[:build_number])
-      config = channel.find_config(version: data[:config_version])
+      release = add_release(data[:release], event: event)
 
-      author = User.new(
-        name: data[:author][:name],
-        username: data[:author][:username],
-        avatar_url: data[:author][:avatar_url],
-      )
-
-      release = channel.add_release(
-        id: data[:id],
-        build: build,
-        config: config,
-        author: author,
-        event: RollbackEvent.new(target_release: target_release)
-      )
-
-      $logger.info "Added release #{release}"
+      $logger.info "Added release #{release} (rolled back to #{target_release})"
     end
 
     def config_change_released(timestamp, data)
       config_key, config_value = data.values_at(:config_key, :config_value)
-      data = data[:release]
+      event = ConfigChangedEvent.new(key: config_key, value: config_value)
+      release = add_release(data[:release], event: event)
+
+      $logger.info "Added release #{release} (config key #{config_key} changed)"
+    end
+
+    private
+
+    def add_release(data, event:)
       project = @repo.find_project(data[:project_id])
       pipeline = project.find_pipeline(data[:pipeline_id])
       channel = pipeline.find_channel(data[:channel_id])
@@ -111,8 +64,23 @@ module Starfish
         build: build,
         config: config,
         author: author,
-        event: ConfigChangedEvent.new(key: config_key, value: config_value)
+        event: event
       )
+    end
+
+    def find_build(data)
+      project = @repo.find_project(data[:project_id])
+      pipeline = project.find_pipeline(data[:pipeline_id])
+
+      pipeline.find_build_by_number(data[:build_number])
+    end
+
+    def find_release(data, target_release_id)
+      project = @repo.find_project(data[:project_id])
+      pipeline = project.find_pipeline(data[:pipeline_id])
+      channel = pipeline.find_channel(data[:channel_id])
+
+      channel.find_release(target_release_id)
     end
   end
 end
