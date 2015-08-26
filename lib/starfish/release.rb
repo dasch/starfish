a90f3b82992b0c5e2d26776d293f49ddc7396fe3
Daniel Schierbeck <dasch@zendesk.com>
2015-08-26 17:43:27 +0200

    Store the timestamp for releases

--- lib/starfish/release.rb
+++ lib/starfish/release.rb
@@ -1,8 +1,8 @@
 module Starfish
   class Release
-    attr_reader :id, :build, :config, :number, :channel, :event
+    attr_reader :id, :build, :config, :number, :channel, :event, :timestamp
 
-    def initialize(id: SecureRandom.uuid, build:, config:, number:, channel:, author:, event:)
+    def initialize(id: SecureRandom.uuid, build:, config:, number:, channel:, author:, event:, timestamp:)
       @id = id
       @build = build
       @config = config
@@ -10,6 +10,7 @@ module Starfish
       @channel = channel
       @author = author
       @event = event
+      @timestamp = timestamp
     end
 
     def event_name
