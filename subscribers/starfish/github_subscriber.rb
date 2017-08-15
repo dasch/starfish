90af627e3e87f7cac789789cd7ed1586ae8d52ac
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2017-08-15 16:52:45 +0200

    Show PR reviews

--- subscribers/starfish/github_subscriber.rb
+++ subscribers/starfish/github_subscriber.rb
@@ -30,6 +30,23 @@ module Starfish
       end
     end
 
+    def github_pull_request_reviewed(timestamp, event)
+      project = @repo.find_project(event.project_id)
+      target_branch = event.target_branch
+      pr_id = event.pull_request_id
+
+      if project.has_pipeline_for_branch?(target_branch)
+        pipeline = project.find_pipeline_by_branch(target_branch)
+
+        pr = pipeline.find_pull_request(pr_id)
+
+        pr.add_review(
+          state: event.state,
+          reviewer: event.reviewer,
+        )
+      end
+    end
+
     def github_pull_request_closed(timestamp, event)
       project = @repo.find_project(event.project_id)
       target_branch = pr.base.ref
